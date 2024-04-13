743d67c286f8f9c0518a56e1e85b83a60f65bb1d
krgarav <krgarav@gmail.com>
2024-04-13 17:55:02 +0530

    Added see all icon

--- component/Bakeries.jsx
+++ component/Bakeries.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Button } from "react-native-paper";
 import CakeModule from "./CakeModule";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 const Bakeries = () => {
   return (
     <View>
-      <View>
-        <Text> Your Taste</Text>
-        <Text> See All</Text>
+      <View style={styles.textContainer}>
+        <Text style={styles.headText}> Your Taste</Text>
+        <View style={styles.seeAll}>
+          <Text style={styles.headText1}>See All</Text>
+          <View style={styles.icon}>
+            <FontAwesome6 name="greater-than" size={12} color="black" />
+          </View>
+        </View>
       </View>
       <ScrollView
         horizontal={true}
         contentContainerStyle={styles.bakerContainer}
+        showsHorizontalScrollIndicator={false}
       >
         <CakeModule />
         <CakeModule />
@@ -29,9 +37,36 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     overflow: "scroll",
     alignItems: "center",
-    // flexWrap : "wrap",
     gap: 20,
     paddingHorizontal: 10,
+  },
+  textContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  headText: {
+    fontWeight: "800",
+    fontSize: 22,
+  },
+  headText1: {
+    fontWeight: "800",
+    fontSize: 22,
+  },
+  icon: {
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor : "lightgrey",
+    marginBottom : 5,
+    padding :10,
+    justifyContent : "center",
+    borderRadius : 50
+  },
+  seeAll: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent : "center"
   },
 });
 
--- screen/Cakes.jsx
+++ screen/Cakes.jsx
@@ -4,12 +4,15 @@ import Eventblock from "../component/eventblock";
 import Bakeries from "../component/Bakeries";
 import Veggiebox from "../component/Veggiebox";
 import PopularOnes from "../component/popularOnes";
-
+import { Ionicons } from "@expo/vector-icons";
 const Cakes = () => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headText}>Pre Order From</Text>
+        <Text style={styles.headText}>
+          Pre Order From
+          <Ionicons name="location-outline" size={20} color="black" />{" "}
+        </Text>
         <Text style={styles.subText}>Connaught Place</Text>
       </View>
 
@@ -48,6 +51,9 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "500",
     color: "#636363",
+    alignContent: "center",
+    justifyContent : "center",
+    alignItems : "center"
   },
   subText: {
     fontSize: 22,
